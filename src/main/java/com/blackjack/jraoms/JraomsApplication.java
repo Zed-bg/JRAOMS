8721c3c9ff04c5c9b3284f00478cc1e0e06d0c12
Zed-bg <zwelwinooprogramming@gmail.com>
2023-07-10 13:34:55 +0630

    Initial commit

---
+++ src/main/java/com/blackjack/jraoms/JraomsApplication.java
@@ -0,0 +1,13 @@
+package com.blackjack.jraoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JraomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JraomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackjack/jraoms/JraomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackjack.jraoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JraomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
